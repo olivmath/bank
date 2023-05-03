24a0cea0b4d7bb0989cb1d96b86f486c84a1017d
Lucas Oliveira <olivmath@protonmail.com>
2023-05-03 00:16:19 -0300

    add: erc 2535

---
+++ src/diamont/Diamond.Storage.Lib.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+library DiamondStorageLib {
+    bytes32 constant DIAMOND_STORAGE_POSITION = keccak256("diamond.storage");
+
+    struct Employee {
+        address employee;
+        uint256 budge;
+    }
+
+    struct Storage {
+        address controller;
+        address[] employeeList;
+        mapping(address => Employee) employees;
+        
+        mapping(bytes4 => address) fnSelectorToFacet;
+        mapping(address => bytes4[]) facetToFnSelector;
+        uint256 fnSelectorLength;
+        bytes4[] allFnSelectors;
+    }
+
+    function getDiamondStorage() internal pure returns (Storage storage ds) {
+        bytes32 storagePosition = DIAMOND_STORAGE_POSITION;
+        assembly {
+            ds.slot := storagePosition
+        }
+    }
+}
