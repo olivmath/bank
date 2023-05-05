5dbcacf518d443cd2cc175fdc6fb624763996f41
Lucas Oliveira <olivmath@protonmail.com>
2023-05-05 18:05:59 -0300

    add: new functions into diamond

--- test/Bank/BaseBank.t.sol
+++ test/Bank/BaseBank.t.sol
@@ -9,6 +9,9 @@ contract BaseBankTest is BaseSetup {
     bytes4 public deleteEmployee;
     bytes4 public getAllEmployees;
     bytes4 public getEmployee;
+    bytes4 public payAllEmployees;
+    bytes4 public getBalance;
+    bytes4 public getTotalEmployeeCost;
 
     uint256 public bobBudge;
     uint256 public aliceBudge;
@@ -24,11 +27,18 @@ contract BaseBankTest is BaseSetup {
         // 0x6e7c4ab1  =>  deleteEmployee(address)
         // 0xe3366fed  =>  getAllEmployees() -> address[] memory
         // 0x32648e09  =>  getEmployee(address) -> (address, uint256)
+        // 0x809e9ef5  =>  payAllEmployees()
+        // 0x12065fe0  =>  getBalance()
+        // 0x1e153139  =>  getTotalEmployeeCost()
+
         createEmployee = 0x520a19c0;
         updateEmployee = 0x5e91d8ec;
         deleteEmployee = 0x6e7c4ab1;
         getAllEmployees = 0xe3366fed;
         getEmployee = 0x32648e09;
+        payAllEmployees = 0x809e9ef5;
+        getBalance = 0x12065fe0;
+        getTotalEmployeeCost = 0x1e153139;
 
         bobBudge = 5000 * 10e18;
         aliceBudge = 20 * 10e18;
