197f9f0f88308acaf2e8bbfeedbb6d62e520ce6c
Lucas Oliveira <olivmath@protonmail.com>
2023-05-08 01:02:55 -0300

    update: move code to another component

--- front/src/components/Bank/index.tsx
+++ front/src/components/Bank/index.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import { Address, createWalletClient, encodePacked, custom } from "viem";
-import { foundry } from "viem/chains";
+import React from "react";
+import { Address } from "viem";
 import "viem/window";
 import { FunctionSelector } from "./FunctionSelector";
 import selectors from "../../config/selectors";
@@ -10,46 +9,45 @@ interface BankProp {
   account: Address;
 }
 
-const walletClient = createWalletClient({
-  chain: foundry,
-  transport: custom(window.ethereum!),
-});
-
 function Bank({ account }: BankProp) {
-  // STATES
-  const contractAddress: Address = "0x1234";
-
-  // FUNCTIONS
-
-  const callFunction = async (
-    funcSelector: string,
-    types: string[],
-    values: any[]
-  ) => {
-    const data = encodePacked(types, [funcSelector, ...values]);
-    const hash = await walletClient.sendTransaction({
-      data: data,
-      account,
-      to: contractAddress,
-    });
-  };
-
   // COMPONENT
 
   return (
     <>
       <styles.ConnectedAccount>Connected: {account}</styles.ConnectedAccount>
       <h1>Bank Contract</h1>
-      <FunctionSelector selector={selectors.createEmployee}></FunctionSelector>
-      <FunctionSelector selector={selectors.updateEmployee}></FunctionSelector>
-      <FunctionSelector selector={selectors.deleteEmployee}></FunctionSelector>
-      <FunctionSelector selector={selectors.getEmployee}></FunctionSelector>
-      <FunctionSelector selector={selectors.getAllEmployees}></FunctionSelector>
-      <FunctionSelector selector={selectors.getBalance}></FunctionSelector>
       <FunctionSelector
+        account={account}
+        selector={selectors.createEmployee}
+      ></FunctionSelector>
+      <FunctionSelector
+        account={account}
+        selector={selectors.updateEmployee}
+      ></FunctionSelector>
+      <FunctionSelector
+        account={account}
+        selector={selectors.deleteEmployee}
+      ></FunctionSelector>
+      <FunctionSelector
+        account={account}
+        selector={selectors.getEmployee}
+      ></FunctionSelector>
+      <FunctionSelector
+        account={account}
+        selector={selectors.getAllEmployees}
+      ></FunctionSelector>
+      <FunctionSelector
+        account={account}
+        selector={selectors.getBalance}
+      ></FunctionSelector>
+      <FunctionSelector
+        account={account}
         selector={selectors.getTotalEmployeeCost}
       ></FunctionSelector>
-      <FunctionSelector selector={selectors.payAllEmployees}></FunctionSelector>
+      <FunctionSelector
+        account={account}
+        selector={selectors.payAllEmployees}
+      ></FunctionSelector>
     </>
   );
 }
