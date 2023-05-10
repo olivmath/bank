c1258dd29dc2dd89ec6ecf42c222a057860038a4
Lucas Oliveira <olivmath@protonmail.com>
2023-05-09 23:44:03 -0300

    add: info about contract bank

--- front/src/components/Bank/index.tsx
+++ front/src/components/Bank/index.tsx
@@ -5,17 +5,40 @@ import { FunctionSelector } from "./FunctionSelector";
 import selectors from "../../config/selectors";
 import styles from "./styles";
 
+const employees = Array(10).fill({ address: "0x0", budge: 10000, bonus: 300 });
+
 interface BankProp {
   account: Address;
 }
 
 function Bank({ account }: BankProp) {
   // COMPONENT
-
   return (
     <>
       <styles.ConnectedAccount>Connected: {account}</styles.ConnectedAccount>
       <h1>Bank Contract</h1>
+      <styles.Container>
+        <div>
+          <h2>Info</h2>
+          <styles.Column1>
+            <p>Total Cost: {1000}</p>
+            <p>Balance: {1200}</p>
+            <p>Total Employess: {employees.length}</p>
+            <p>Total Bonus: {200}</p>
+          </styles.Column1>
+        </div>
+        <div>
+          <h2>Employees</h2>
+          <styles.Column2>
+            {employees.map((employee, index) => (
+              <p key={index}>
+                Address: {employee.address}, Budget: {employee.budge}, Bonus:{" "}
+                {employee.bonus}
+              </p>
+            ))}
+          </styles.Column2>
+        </div>
+      </styles.Container>
       <FunctionSelector
         account={account}
         selector={selectors.createEmployee}
@@ -27,26 +50,6 @@ function Bank({ account }: BankProp) {
       <FunctionSelector
         account={account}
         selector={selectors.deleteEmployee}
-      ></FunctionSelector>
-      <FunctionSelector
-        account={account}
-        selector={selectors.getEmployee}
-      ></FunctionSelector>
-      <FunctionSelector
-        account={account}
-        selector={selectors.getAllEmployees}
-      ></FunctionSelector>
-      <FunctionSelector
-        account={account}
-        selector={selectors.getBalance}
-      ></FunctionSelector>
-      <FunctionSelector
-        account={account}
-        selector={selectors.getTotalEmployeeCost}
-      ></FunctionSelector>
-      <FunctionSelector
-        account={account}
-        selector={selectors.payAllEmployees}
       ></FunctionSelector>
     </>
   );
--- front/src/components/Bank/styles.ts
+++ front/src/components/Bank/styles.ts
@@ -1,5 +1,19 @@
 import styled from "styled-components";
 
+const Column = styled.div`
+  flex: 1;
+`;
+
+const Column1 = styled(Column)`
+  margin-right: 10px;
+`;
+
+const Column2 = styled(Column)`
+  margin-left: 10px;
+  height: 150px;
+  overflow-y: scroll;
+`;
+
 export default {
   ConnectedAccount: styled.div`
     font-size: 24px;
@@ -13,4 +27,13 @@ export default {
     padding: 8px;
     background-color: #007bff;
   `,
+  Container: styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+  `,
+  Column,
+  Column1,
+  Column2,
 };
