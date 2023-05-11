2ae8bbb795c0768cbf054cf51509e5c5c098127e
Lucas Oliveira <olivmath@protonmail.com>
2023-05-11 17:09:47 -0300

    add: create employee componente

---
+++ front/src/components/Bank/ContractFunctions/styles.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export default {
+  Wrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 16px;
+  `,
+  FunctionName: styled.h2`
+    margin-bottom: 8px;
+    font-weight: bold;
+  `,
+  ParamsWrapper: styled.div`
+    display: flex;
+    align-items: flex-end;
+  `,
+  InputWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-right: 16px;
+  `,
+  InputLabel: styled.label`
+    font-size: 12px;
+    color: #555;
+    margin-bottom: 4px;
+  `,
+  InputText: styled.input`
+    width: 400px;
+    padding: 6px 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 14px;
+  `,
+  Input: styled.input`
+    width: 200px;
+    padding: 6px 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 14px;
+  `,
+  SendButton: styled.button`
+    padding: 6px 12px;
+    background-color: ${({ disabled }) => (disabled ? "#ccc" : "#007bff")};
+    color: #fff;
+    border: 1px solid ${({ disabled }) => (disabled ? "#ccc" : "#007bff")};
+    border-radius: 4px;
+    cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+    transition: background-color 0.2s;
+
+    &:hover {
+      background-color: ${({ disabled }) => (disabled ? "#ccc" : "#0056b3")};
+      border-color: ${({ disabled }) => (disabled ? "#ccc" : "#0056b3")};
+    }
+  `,
+};
