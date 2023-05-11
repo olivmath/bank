37ebed57323541d44c586b301a84f732ba1aff72
Lucas Oliveira <olivmath@protonmail.com>
2023-05-11 12:54:25 -0300

    add: use effect for fetch data

--- front/src/components/Employee/index.tsx
+++ front/src/components/Employee/index.tsx
@@ -1,21 +1,43 @@
-import React from "react";
-import { Address } from "viem";
+import React, { useEffect, useState } from "react";
+import { Address, createPublicClient, http } from "viem";
 import Styles from "./styles";
+import { foundry } from "viem/chains";
+import contracts from "../../config/contracts";
 
 interface EmployeeProps {
   account: Address;
 }
 
+const client = createPublicClient({
+  chain: foundry,
+  transport: http(),
+});
+
 function Employees({ account }: EmployeeProps) {
-  // Busque informações do funcionário (budge e bonus) e defina o estado
-  const [budge, bonus] = [123, 0];
+  const [budge, setBudge] = useState<Address>();
+  const [bonus, setBonus] = useState<number>();
+
+  useEffect(() => {
+    function getEmployee() {
+      const data = client.readContract({
+        abi: contracts.facet_bank.abi,
+        address: contracts.diamond.address,
+        functionName: "getEmployee",
+        args: [account],
+      });
+
+      console.log(data);
+
+      setBudge(data[0]);
+      setBonus(data[1]);
+    }
+    getEmployee();
+  }, []);
 
   return (
     <>
       <div>
-        <Styles.ConnectedAccount>
-          Connected: {account}
-        </Styles.ConnectedAccount>
+        <Styles.ConnectedAccount>Connected: {account}</Styles.ConnectedAccount>
         <Styles.Title>Employee {account}</Styles.Title>
         <Styles.Info>Your budge: {budge}</Styles.Info>
         <Styles.Info>Have bonus: {bonus}</Styles.Info>
