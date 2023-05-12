3f7004fdcec5ebda70702c539de5b60da4cb531d
Lucas Oliveira <olivmath@protonmail.com>
2023-05-12 17:23:26 -0300

    add: loupe tests

--- test/Diamond/Louper.t.sol
+++ test/Diamond/Louper.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Facet} from "../../src/diamond/interfaces/Facet.sol";
+import {Facet} from "../../src/diamond/interfaces/Facet.types.sol";
 import {BaseSetup} from "../BaseSetup.sol";
 
 contract LouperTest is BaseSetup {
@@ -9,5 +9,31 @@ contract LouperTest is BaseSetup {
         BaseSetup.setUp();
     }
 
-    function testGetFacets() public {}
+    function testGetAddressListOfFacets() public {
+        address[] memory facets = diamond.facetAddresses();
+
+        assertEq(facets.length, 1, "Number of facets should be 1");
+    }
+
+    function testGetAddressListOfFunctions() public {
+        bytes4[] memory fnSelectors = diamond.facetFunctionSelectors(address(bank));
+
+        assertEq(fnSelectors.length, 8, "Number of facets should be 8");
+    }
+
+    function testGetFacetAddressbyFnSelectors() public {
+        bytes4[] memory fnSelectors = diamond.facetFunctionSelectors(address(bank));
+
+        for (uint256 i = 0; i < fnSelectors.length; i++) {
+            address bankAddr = diamond.facetAddress(fnSelectors[i]);
+
+            assertEq(bankAddr, address(bank), "Should return Bank address");
+        }
+    }
+
+    function testGetFacets() public {
+        Facet[] memory facets = diamond.facets();
+
+        assertEq(facets.length, 1, "Should have 1 facet");
+    }
 }
