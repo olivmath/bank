6ad6149eaf014ab6e0139a80161257013caa785f
Lucas Oliveira <olivmath@protonmail.com>
2023-05-11 22:43:21 -0300

    add: buttons to manage balnace of bank

--- front/src/components/Bank/ContractFunctions/styles.ts
+++ front/src/components/Bank/ContractFunctions/styles.ts
@@ -52,4 +52,21 @@ export default {
       border-color: ${({ disabled }) => (disabled ? "#ccc" : "#0056b3")};
     }
   `,
+
+  PayButton: styled.button`
+    margin: auto;
+    padding: 8px 16px;
+    background-color: #007bff;
+    color: #fff;
+    border: 1px solid #007bff;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 16px;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background-color: #0056b3;
+      border-color: #0056b3;
+    }
+  `,
 };
